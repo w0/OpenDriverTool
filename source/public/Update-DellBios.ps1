4224201066d99c04f5117cc8386c09decb68f2f3
w0 <33212583+w0@users.noreply.github.com>
2023-07-18 19:43:52 -0500

    oops

--- source/public/Update-DellBios.ps1
+++ source/public/Update-DellBios.ps1
@@ -1,15 +1,10 @@
-function Update-DellDriver {
+function Update-DellBios {
     [CmdletBinding()]
     param (
 
         [Parameter(Mandatory)]
         [string]
         $Model,
-
-        [Parameter(Mandatory)]
-        [ValidateSet('Windows 10', 'Windows 11')]
-        [string]
-        $OSVersion,
 
         [Parameter()]
         [io.directoryInfo]
@@ -36,63 +31,70 @@ function Update-DellDriver {
         $WorkingDir = Path-Creator -Parent $env:TEMP -Child 'OpenDriverTool'
     }
 
-    "Updating drivers for Dell $Model" | Log
+    "Updating Bios for Dell $Model" | Log
 
     Path-Creator -Parent $WorkingDir -Child 'Content' | Out-Null
     
     $DellCatalog = Get-DellCatalog
 
     $DriverPackCatalogUrl = $DellCatalog.Where( {$_.Filename -eq 'DriverPackCatalog.CAB'}, 'First' )
+    $CatalogPCUrl      = $DellCatalog.Where( { $_.Filename -eq 'CatalogPC.cab'}, 'First' )
+    
 
     '  Getting Dell cabinet files...' | Log
     '    Downloading: {0}' -f $DriverPackCatalogUrl.Url | Log
     $DriverPackCatalogCAB = $DriverPackCatalogUrl.Url | Get-RemoteFile -Destination $WorkingDir
 
+    '    Downloading: {0}' -f $CatalogPCUrl.Url | Log
+    $CatalogPCCAB = $CatalogPCUrl.Url | Get-RemoteFile -Destination $WorkingDir
+
     Expand-Cab -Path $DriverPackCatalogCAB -Destination $WorkingDir\Content
+    Expand-Cab -Path $CatalogPCCAB -Destination $WorkingDir\Content
 
     [xml] $DriverPackCatalog = Get-Content -Path "$WorkingDir\Content\DriverPackCatalog.xml"
+    [xml] $CatalogPC = Get-Content -Path "$WorkingDir\Content\CatalogPC.xml"
 
-    $Driver = Find-DellDrivers -DriverPackCatalog $DriverPackCatalog -Model $Model -OSVersion ($OSVersion -replace '\s', '')
-    '  Found Driver: {0}' -f $Driver.path | Log
+    $BIOS = Find-DellBIOS -Model $Model -DriverPackCatalog $DriverPackCatalog -CatalogPC $CatalogPC
+    '  Found BIOS: {0}' -f $BIOS.path | Log
 
-    $DriverPackage = @{
-        Name = 'Drivers - {0} {1} - {2} {3}' -f 'Dell', $Model, $OSVersion, $Driver.SupportedOperatingSystems.OperatingSystem.osArch
-        Version = $Driver.dellVersion
+    $BIOSPackage = @{
+        Name = 'BIOS Update - {0} {1}' -f 'Dell', $Model
         Manufacturer = 'Dell'
-        Description = '(Models included:{0})' -f ($Driver.SupportedSystems.brand.model.systemID -join ';')
+        Version = $BIOS.dellVersion
+        Description = '(Models included:{0})' -f ($BIOS.SupportedSystems.Brand.Model.systemID -join ';')
     }
 
     Connect-SCCM -SiteCode $SiteCode -SiteServerFQDN $SiteServerFQDN
 
-    if (-not (Confirm-ExistingPackage -Package $DriverPackage -SiteCode $SiteCode) -and $Driver) {
+    if (-not (Confirm-ExistingPackage -Package $BIOSPackage -SiteCode $SiteCode) -and $BIOS) {
 
-        "`n  Driver version not found in configmgr" | Log
-        
-        '    Downloading: {0}' -f $Driver.path  | Log
-        $DriverFile = Get-RemoteFile -Url ('{0}/{1}' -f 'https://downloads.dell.com', $Driver.path) -Destination $WorkingDir -Hash $Drivers.hashMD5 -Algorithm MD5
+        $Flash64Url = 'https://downloads.dell.com/FOLDER08405216M/1/Ver3.3.16.zip'
 
-        '    Extracting: {0}' -f $DriverFile.FullName| Log
-        $ExtractedContent = Expand-Drivers -Make 'Dell' -Path $DriverFile -Destination $WorkingDir
+        "`n  BIOS version not found in configmgr" | Log
 
-        '    Compressing driver package.'
-        $Archive = Compress-Drivers -ContentPath $ExtractedContent
+        '    Downloading: {0}' -f $BIOS.path | Log
+        $BIOSFile = Get-RemoteFile -Url ('{0}/{1}' -f 'https://downloads.dell.com', $BIOS.path) -Destination $WorkingDir -Hash $BIOS.hashMD5 -Algorithm MD5
 
-        $DriverPath = (
+        '    Downloading: {0}' -f $Flash64Url | Log
+        $Flash64Zip = Get-RemoteFile -Url $Flash64Url -Destination $WorkingDir
+
+        $Flash64Extract = Expand-Archive -Path $Flash64Zip -DestinationPath $WorkingDir -Force -PassThru | Where-Object Name -eq 'Flash64W.exe'
+
+        $BIOSPath = (
             'Dell',
             $Model,
-            'Driver',
-            $OSVersion,
-            $Driver.dellVersion
+            'BIOS',
+            $BIOS.dellVersion
         )
 
-        $DriverContent = Path-Creator -Parent $ContentShare -Child $DriverPath
+        $BIOSContent = Path-Creator -Parent $ContentShare -Child $BIOSPath
 
-        Copy-Item -Path $Archive -Destination $DriverContent
+        Copy-Item -Path $BIOSFile -Destination $BIOSContent
+        Copy-Item -Path $Flash64Extract -Destination $BIOSContent
 
-
-        '    Creating driver package in sccm.'
-        New-Package -Package $DriverPackage -Type 'Driver' -SiteCode $SiteCode -ContentPath $DriverContent -DistributionPoints $DistributionPoints -Make 'Dell'
+        '    Creating bios package in sccm.'
+        New-Package -Package $BIOSPackage -Type 'BIOS' -SiteCode $SiteCode -ContentPath $BIOSContent -DistributionPoints $DistributionPoints -Make $Make
     } else {
-        '  Latest driver already in confimgr.' | Log
+        '  Latest bios already in configmgr.' | Log
     }
 }
